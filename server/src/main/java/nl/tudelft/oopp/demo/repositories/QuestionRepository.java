81c02c07825c5e0cea80161a054f5342a156429d
Emiel Witting <e.a.witting@student.tudelft.nl>
2021-03-04 17:24:39 +0100

    add getQuestions endpoint

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/QuestionRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/QuestionRepository.java
@@ -4,9 +4,9 @@ import nl.tudelft.oopp.demo.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
     @Query("SELECT content FROM Question")
-    Collection<Question> findAll(String query);
+    List<String> getAllStrings();
 }
