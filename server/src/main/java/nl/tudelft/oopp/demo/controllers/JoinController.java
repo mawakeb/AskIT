ba131caef505a58638a3d33eb235ac7e79a7c894
Cherin Kim <C.kim-1@student.tudelft.nl>
2021-03-05 09:43:04 +0100

    Add JoinController to handle /join requests
    allow joining a room with a link

---
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/JoinController.java
@@ -0,0 +1,43 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping({"join"})
+public class JoinController {
+    @Autowired
+    private RoomRepository repo;
+
+    public JoinController() {
+    }
+
+    @GetMapping({"link"})
+    @ResponseBody
+    public void joinLink(@RequestParam String q) {
+        String[] links = q.split("/");
+        long id = Long.parseLong(links[0]);
+        String role = links[1];
+        Room room = this.repo.findByid(id);
+        if (room != null) {
+            if (role.equals(room.getStudent())) {
+                System.out.println("You are a student");
+            } else {
+                if (!role.equals(room.getStaff())) {
+                    return;
+                }
+
+                System.out.println("You are a staff");
+            }
+
+            System.out.println("Successfully joined");
+        }
+    }
+}
+
