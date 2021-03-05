d80e6acbe6816163121c3d6050b135cca8045ff8
Cherin Kim <C.kim-1@student.tudelft.nl>
2021-03-05 09:39:18 +0100

    Add RoomController to handle /create requests

---
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/RoomController.java
@@ -0,0 +1,59 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import nl.tudelft.oopp.demo.entities.Room;
+import nl.tudelft.oopp.demo.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping({"create"})
+public class RoomController {
+    private long roomId = 0L;
+    @Autowired
+    private RoomRepository repo;
+
+    public RoomController() {
+    }
+
+    @GetMapping({"name"})
+    @ResponseBody
+    public List<String> createLink(@RequestParam String q) {
+        String string1 = randomString();
+
+        String string2;
+        for(string2 = randomString(); string1.equals(string2); string2 = randomString()) {
+        }
+
+        ++this.roomId;
+        Room newRoom = new Room(this.roomId, q, string1, string2);
+        this.repo.save(newRoom);
+        string1 = this.roomId + "/" + string1;
+        string2 = this.roomId + "/" + string2;
+        System.out.println(string1);
+        System.out.println(string2);
+        List<String> links = new ArrayList();
+        links.add(string1);
+        links.add(string2);
+        return links;
+    }
+
+    public static String randomString() {
+        String result = "";
+        Random random = new Random();
+        char[] characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+
+        for(int i = 0; i < 15; ++i) {
+            int randomInt = random.nextInt(characters.length);
+            result = result + characters[randomInt];
+        }
+
+        return result;
+    }
+}
