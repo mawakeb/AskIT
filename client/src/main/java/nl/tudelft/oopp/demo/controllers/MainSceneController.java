b690a599daba4a87a1aa6fb6e905232ce40d5430
Cherin Kim <C.kim-1@student.tudelft.nl>
2021-03-05 09:50:30 +0100

    Add methods in MainSceneController to allow interaction with UI

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -11,16 +11,44 @@ import java.util.List;
 
 public class MainSceneController {
     @FXML
-    private TextField search;
+    private TextField question;
     @FXML
-    private ListView<Quote> quoteList;
+    private TextField name;
+    @FXML
+    private TextField link;
+    @FXML
+    private ListView<String> list;
+
 
     /**
      * Handles clicking the button.
      */
     public void buttonClicked() {
-        ServerCommunication.sendQuestion(search.getText());
+        ServerCommunication.sendQuestion(question.getText());
+        question.clear();
 
+    }
 
+    /**
+     * Handles clicking the create button
+     */
+    public void createButtonClicked() {
+        List<String> links = ServerCommunication.createRoom(name.getText());
+        name.clear();
+        list.getItems().clear();
+        list.getItems().add("staff link: ");
+        list.getItems().add(links.get(0));
+        list.getItems().add("student link: ");
+        list.getItems().add(links.get(1));
+
+        //TODO: copy onclick
+    }
+
+    /**
+     * Handles clicking the join button
+     */
+    public void joinButtonClicked(){
+        ServerCommunication.joinRoom(link.getText());
+        link.clear();
     }
 }
