6d4ffc7cf7a32d4018d390d2171d2e6400f337d7
Cherin Kim <C.kim-1@student.tudelft.nl>
2021-03-09 21:51:34 +0100

    Draft of mainScene
    Changed colors and alignments

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
@@ -7,6 +8,10 @@ import javafx.scene.control.TextField;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.data.Quote;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.util.List;
 
 public class MainSceneController {
@@ -36,12 +41,25 @@ public class MainSceneController {
         List<String> links = ServerCommunication.createRoom(name.getText());
         name.clear();
         list.getItems().clear();
-        list.getItems().add("staff link: ");
-        list.getItems().add(links.get(0));
-        list.getItems().add("student link: ");
-        list.getItems().add(links.get(1));
+        if (links.size()==1){
+            list.getItems().add("error " + links.get(0));
+        } else {
+            list.getItems().add("Click on link (individually) to copy to clipboard");
+            list.getItems().add("staff link: ");
+            list.getItems().add(links.get(0));
+            list.getItems().add("student link: ");
+            list.getItems().add(links.get(1));
+        }
+    }
 
-        //TODO: copy onclick
+    /**
+     * Copy the link to the clipboard when the list is clicked
+     */
+    public void copyList() {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String link = list.getSelectionModel().getSelectedItem();
+        StringSelection linkCopy = new StringSelection(link);
+        clipboard.setContents(linkCopy, null);
     }
 
     /**
