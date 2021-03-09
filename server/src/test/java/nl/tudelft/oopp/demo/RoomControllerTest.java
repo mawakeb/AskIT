4645a74f90a00e7b198e52b985b16e6f0dbebeb8
Ebenezer Fosu <E.Fosu@student.tudelft.nl>
2021-03-09 01:38:00 +0100

    Add a test for RoomController class

---
+++ server/src/test/java/nl/tudelft/oopp/demo/RoomControllerTest.java
@@ -0,0 +1,30 @@
+package nl.tudelft.oopp.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import nl.tudelft.oopp.demo.repositories.RoomRepository;
+import org.junit.jupiter.api.Test;
+
+import nl.tudelft.oopp.demo.controllers.RoomController;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ExtendWith(MockitoExtension.class)
+class RoomControllerTest {
+    @Mock
+    RoomRepository roomRepository;
+
+    @Test
+    public void checkIfRandomStringGetsCreated(){
+        String randString = RoomController.randomString();
+
+        assertEquals(randString.length(), 15);
+        assertNotNull(randString);
+    }
+
+
+
+}
