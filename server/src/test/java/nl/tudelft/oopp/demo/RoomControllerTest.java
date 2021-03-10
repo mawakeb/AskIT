1b5fe39284d9bdfb94ea093af715b80439404a90
Ebenezer Fosu <E.Fosu@student.tudelft.nl>
2021-03-10 10:17:41 +0100

    Attempted test for RoomController

--- server/src/test/java/nl/tudelft/oopp/demo/RoomControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/RoomControllerTest.java
@@ -3,6 +3,7 @@ package nl.tudelft.oopp.demo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import nl.tudelft.oopp.demo.entities.Room;
 import nl.tudelft.oopp.demo.repositories.RoomRepository;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.http.HttpResponse;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class RoomControllerTest {
@@ -25,6 +29,17 @@ class RoomControllerTest {
         assertNotNull(randString);
     }
 
+    @Test
+    public void checkIfTwoValidLinksAreReturned(){
 
+        RoomController rc = new RoomController();
+
+        Room testRoom = new Room(1,"TestRoom","1/123","1/456");
+
+        when(roomRepository.save(any(Room.class))).thenReturn(testRoom);
+
+        List<String> testLinks = rc.createLink("TestRoom");
+        assertEquals(testLinks.size(), 2);
+    }
 
 }
