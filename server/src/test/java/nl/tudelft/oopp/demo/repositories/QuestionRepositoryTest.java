a075c526e75372b8164a07004b15536c85fa5a85
Dāvis Kažemaks <D.Kazemaks@student.tudelft.nl>
2021-03-12 20:38:39 +0100

    adding dependency for testing

---
+++ server/src/test/java/nl/tudelft/oopp/demo/repositories/QuestionRepositoryTest.java
@@ -0,0 +1,25 @@
+package nl.tudelft.oopp.demo.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+
+class QuestionRepositoryTest {
+    // further info: https://www.baeldung.com/spring-boot-testing
+
+    @Test
+    void getAllStrings() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
