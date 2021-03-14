04665f23c721eb92a09643f5780c1d3e2002b219
Emiel Witting <e.a.witting@student.tudelft.nl>
2021-03-14 13:22:36 +0100

    disable sendButton when room is closed and reformat RoomSceneController to use updateAll

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/RoomSceneController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
@@ -17,6 +18,8 @@ public class RoomSceneController {
     private TextArea question;
     @FXML
     private ListView<Question> questionList;
+    @FXML
+    private Button sendButton;
 
     /**
      * Use @FXML initialize() instead of constructor.
@@ -29,7 +32,7 @@ public class RoomSceneController {
         questionList.setCellFactory((Callback<ListView<Question>, ListCell<Question>>) params -> {
             return new QuestionCell(this);
         });
-        updateQuestionList();
+        updateAll();
     }
 
     /**
@@ -37,7 +40,7 @@ public class RoomSceneController {
      */
     public void sendButtonClicked() {
         ServerCommunication.sendQuestion(question.getText());
-        updateQuestionList();
+        updateAll();
         question.clear();
     }
 
@@ -61,5 +64,21 @@ public class RoomSceneController {
         List<Question> questions = ServerCommunication.getQuestions();
         questionList.getItems().clear();
         questionList.getItems().addAll(questions);
+    }
+
+    /**
+     * Gets the status of the room and updates the UI accordingly.
+     */
+    public void updateRoomStatus() {
+        boolean isOpen = ServerCommunication.getRoomStatus();
+        sendButton.setDisable(!isOpen);
+    }
+
+    /**
+     * Gets all possible updates from the server.
+     */
+    public void updateAll() {
+        updateQuestionList();
+        updateRoomStatus();
     }
 }
