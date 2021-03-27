96d9c6d5828d913d67f7421464c0cf3f7a9e6e71
Dāvis Kažemaks <D.Kazemaks@student.tudelft.nl>
2021-03-27 01:40:45 +0100

    added tests for joining a room serverside

--- server/src/test/java/nl/tudelft/oopp/askit/controllers/RoomControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/askit/controllers/RoomControllerTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.oopp.askit.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -20,6 +21,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 @ExtendWith(MockitoExtension.class)
 class RoomControllerTest {
@@ -51,6 +53,41 @@ class RoomControllerTest {
 
         assertEquals(links.get(0).length(), 52);
         assertEquals(links.get(1).length(), 52);
+    }
+
+    @Test
+    void joinRoomStudent() {
+        // sets up a response
+        when(roomRepository.findByid(id)).thenReturn(room);
+
+        String links = id.toString() + "/student";
+        List<String> list = rc.joinLink(links);
+
+        assertEquals(list, List.of(room.getName(),"student", room.getOpenTime().toString()));
+    }
+
+    @Test
+    void joinRoomStaff() {
+        // sets up a response
+        when(roomRepository.findByid(id)).thenReturn(room);
+
+        String links = id.toString() + "/staff";
+        List<String> list = rc.joinLink(links);
+
+        assertEquals(list, List.of(room.getName(),"staff", room.getOpenTime().toString()));
+    }
+
+    @Test
+    void joinRoomIncorrectRole() {
+        // sets up a response
+        when(roomRepository.findByid(id)).thenReturn(room);
+
+        String links = id.toString() + "/bad";
+
+        // Must throw an ResponseStatusException
+        assertThrows(ResponseStatusException.class, () -> {
+            rc.joinLink(links);
+        });
     }
 
     @Test
